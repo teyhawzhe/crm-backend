2d01dfaa6d67149e55fb3d968c6ff663b9a883c7
teyhawzhe <zhe_215@hotmail.com>
2019-09-16 21:11:13 +0800

    cors

--- src/main/java/com/crm/config/WebSecurityConfig.java
+++ src/main/java/com/crm/config/WebSecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -61,8 +62,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		// super.configure(http);
+		
+		
 		http.cors().disable();
-		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated()
+		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()
+		.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+		.anyRequest().authenticated()
 				.and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
